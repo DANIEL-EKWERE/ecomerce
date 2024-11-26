5bf1620e4e77cf43e8038b3e43dc54ef7407e540
DANIEL-EKWERE <102237731+DANIEL-EKWERE@users.noreply.github.com>
2024-11-26 11:19:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-# exist on error
-
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
 pip install gunicorn
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Apply migrations
 python manage.py migrate
+
+# Create superuser
+echo "from django.contrib.auth.models import User; \
+if not User.objects.filter(username='admin').exists(): \
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')" | python manage.py shell
